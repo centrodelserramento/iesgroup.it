8eed324b9db5b832a231321d835599cfcf061160
Andrea Zonca <code@andreazonca.com>
2026-05-06 19:53:52 -0700

    chore: configure for custom domain on GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: process.env.NETLIFY ? '/' : '/iesgroup.it/',
+  base: '/',
   server: {
     host: '0.0.0.0',
     port: 5173
