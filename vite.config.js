87636e25f339a5f24ae234fbadb0f7c65403fccf
Andrea Zonca <code@andreazonca.com>
2026-05-07 00:02:09 -0700

    fix: include privacy and terms pages in the build process

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        privacy: resolve(__dirname, 'privacy.html'),
+        terms: resolve(__dirname, 'terms.html'),
+      },
+    },
+  },
   server: {
     host: '0.0.0.0',
     port: 5173
